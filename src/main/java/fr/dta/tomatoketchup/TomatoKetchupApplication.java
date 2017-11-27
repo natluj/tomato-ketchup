5118ca94be10033dbba5a61d12b13312c4cdf8f4
Julie TAN <jtan@DIGINAMIC.LOCAL>
2017-11-27 09:45:23 +0100

    init

---
+++ src/main/java/fr/dta/tomatoketchup/TomatoKetchupApplication.java
@@ -0,0 +1,12 @@
+package fr.dta.tomatoketchup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomatoKetchupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomatoKetchupApplication.class, args);
+	}
+}
